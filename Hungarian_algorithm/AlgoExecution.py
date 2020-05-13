5417842c9b8cd1f67c7d4b829e59ae698a6e0a53
Kruk Timofey <kruktimofey@gmail.com>
2020-05-13 15:48:56 +0300

    README&imports written

---
+++ Hungarian_algorithm/AlgoExecution.py
@@ -0,0 +1,8 @@
+import Algorithm
+import DataFormatting
+
+
+if __name__ == "__main__":
+    """Here a given file will be formatted into matrix.
+    Then algorithm will be executed and results returned"""
+    pass
