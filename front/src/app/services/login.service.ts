da719c668fbe245ac729851a03722b9cf9be6280
Nikita Tev <tev.forwork@gmail.com>
2020-05-31 18:22:25 +0300

    rework login component

--- front/src/app/services/login.service.ts
+++ front/src/app/services/login.service.ts
@@ -1,14 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {MockServerService} from "../mock-server/mockServer.servise";
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({providedIn: "root"})
 export class LoginService {
   constructor(private http: HttpClient,
               private server: MockServerService) {}
 
-  public validateStudent(){
-    return false;
+  public validateStudent(rlogin, rpassword){
+    return this.http.post("http://localhost:8080/auth/login", { login: rlogin, password: rpassword });
   }
 
 }
